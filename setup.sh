a55503465ebaf6357397ca856f2e2b319d907ea4
Liam Colle <liam.colle@epitech.eu>
2025-03-12 13:33:48 +0100

    feat: Update files

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@ echo "Please install neovim and then exit this shell."
 bash
 mkdir ~/.config/nvim
 cp ./init.lua ~/.config/nvim/
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 echo "Please run :PlugInstall inside nvim command line..."
 bash -c nvim
 #nvim --headless +PlugInstall +q
